fbf1de272603f386e695e937dda3494003595431
John Croisant <jacius@gmail.com>
2009-12-14 14:34:52 -0600

    Don't use String#end_with?, it's not stdlib.

--- src/uri.rb
+++ src/uri.rb
@@ -23,7 +23,7 @@ class URI::Generic
 
   def join( *args )
     s = self.to_s
-    s.slice!(-1) if s.end_with?('/')
+    s.slice!(-1) if s[-1] == ?/
     URI.parse( args.unshift(s).join('/') )
   end
 
