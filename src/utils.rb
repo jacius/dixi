11c625d04109566c5653254c355e6a1053537fe7
John Croisant <jacius@gmail.com>
2009-12-27 02:05:09 -0600

    Added Dixi::Utils.snip. Nicely truncates a block of text.

--- src/utils.rb
+++ src/utils.rb
@@ -74,6 +74,52 @@ module Dixi
       { new_path => children }
     end
 
+
+    # Nicely truncate some text, trying to find a natural breaking
+    # point. max_chars is the maximum string length to return.
+    # 
+    # Truncates at (in order of preference):
+    # 
+    # 1. The first paragraph break (two newlines) before the limit.
+    # 2. The last end of sentence (".", "?", or "!") before the limit.
+    # 3. The last end of clause (",", ";", or ":"]) before the limit.
+    # 4. The last whitespace character before the limit.
+    # 5. Or, the max_chars limit.
+    # 
+    def snip( text, max_chars=300 )
+      short = text.slice(0, max_chars)
+
+      # Truncate after the first paragraph break, if there is one.
+      para = short.index(/\n\s*\n/m)
+      return short[0,para] if para
+
+      # Find the last end of sentence, end of clause, or space.
+      sentence = short.rindex(/[.?!](\s|$)/)+1 rescue 0
+      clause   = short.rindex(/[,;:](\s|$)/)+1 rescue 0
+      space    = short.rindex(/\s/)            || 0
+
+      # A position greater than this counts as "near the end".
+      near_end = (short.length * 0.7).to_i
+      half     = (short.length * 0.5).to_i
+
+      # Somewhat fuzzy rules for finding a "natural breaking point" in
+      # the text. Ideally a sentence end, clause end, or word break
+      # near the end of the text. Otherwise, one after halfway.
+      point = if    sentence >= near_end;   sentence
+              elsif clause   >= near_end;   clause
+              elsif space    >= near_end;   space
+              elsif sentence >= half;       sentence
+              elsif clause   >= half;       clause
+              elsif space    >= half;       space
+              end
+
+      # If no break point was found, just return it all.
+      return short unless point
+
+      short[0,point].chomp
+    end
+
+
   end
 end
 
