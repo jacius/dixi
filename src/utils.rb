1cceeef54e7d09c6fc80a024f54f936058f1b07f
John Croisant <jacius@gmail.com>
2010-01-02 03:29:33 -0600

    Removed Dixi::Utils.ls_r, because it won't be used anymore.

--- src/utils.rb
+++ src/utils.rb
@@ -18,62 +18,6 @@
 module Dixi
   module Utils
 
-    # Return a hash of directory contents, recursively.
-    # 
-    # Given a directory structure like this:
-    # 
-    # * a/
-    #   * b.txt
-    #   * c/
-    #     * d.txt
-    #     * e/
-    # 
-    # `ls_r( Pathname.new("a") )` will produce:
-    # 
-    #   { #<Pathname:a> => {
-    #       #<Pathname:a/b.txt> => nil,
-    #       #<Pathname:a/c> => {
-    #         #<Pathname:a/c/d.txt> => nil,
-    #         #<Pathname:a/c/e> => {}
-    #       }
-    #     }
-    #   }
-    # 
-    # The path arg must be an instance of Pathname.
-    # 
-    # If a block is given, each Pathname will be passed to the block
-    # and the result used in the hash instead of the Pathname. This
-    # way, you can process the results as they are built, so you get a
-    # hash of things besides Pathnames.
-    # 
-    # Symlinks are not followed.
-    # 
-    def ls_r( path, &block )
-      new_path = path
-
-      if block_given?
-        new_path = block.call(path)
-        return {} if new_path.nil?
-      end
-
-      children = nil
-
-      if path.directory?
-        # Call this method on each child. Makes an array of hashes.
-        children = path.children.collect{ |child|
-          ls_r( child, &block )
-        }
-        # Combine the array of hashes into a single hash.
-        # But ignore any children that are nil.
-        children = children.reduce({}){ |mem,child_hash|
-          mem.merge!( child_hash.reject{ |k,v| k.nil? } )
-          mem
-        }
-      end
-
-      { new_path => children }
-    end
-
 
     # Nicely truncate some text, trying to find a natural breaking
     # point. max_chars is the maximum string length to return.
@@ -123,5 +67,5 @@ module Dixi
   end
 end
 
-# You can do either Dixi::Utils.ls_r or include the module and do ls_r.
+# You can do "Dixi::Utils.foo" or include Dixi::Utils and do "foo"
 Dixi::Utils.extend( Dixi::Utils )
